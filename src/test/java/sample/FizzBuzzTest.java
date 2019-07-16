f3ae838ee29831c2a1ba3e79aaa09c4d74122a80
nagais <nagais@istyle.co.jp>
2019-07-16 17:00:33 +0900

    fiz : assert rouletteのアンチパターンテストの追加

--- src/test/java/sample/FizzBuzzTest.java
+++ src/test/java/sample/FizzBuzzTest.java
@@ -31,4 +31,35 @@ class FizzBuzzTest {
     void postIntThreeReturnStrThree() {
         assertEquals("Fizz", fizzBuzz.convert(3));
     }
+
+    /**
+     * Assert roulette アンチパターン
+     */
+    @Test
+    void testNumber() {
+        assertEquals("1", fizzBuzz.convert(1));
+        System.out.println("test1");
+
+        // コメントアウトを外すと次のassertが検証されない
+        // assertEquals("3", fizzBuzz.convert(2));
+
+        assertEquals("2", fizzBuzz.convert(2));
+        System.out.println("test2");
+    }
+
+    /**
+     * アンチパターンの解法
+     */
+    @Test
+    void testNumber1() {
+        assertEquals("1", fizzBuzz.convert(1));
+    }
+
+    /**
+     * アンチパターンの解法
+     */
+    @Test
+    void testNumber2() {
+        assertEquals("2", fizzBuzz.convert(2));
+    }
 }
