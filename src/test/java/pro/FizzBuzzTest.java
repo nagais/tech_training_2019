fb48cf32b86aaa7431a5efc932ecc9265dc79bed
nagais <nagais@istyle.co.jp>
2019-07-18 14:07:20 +0900

    fix : テストコード変更

--- src/test/java/pro/FizzBuzzTest.java
+++ src/test/java/pro/FizzBuzzTest.java
@@ -1,6 +1,6 @@
 package pro;
 
-import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,79 +8,47 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FizzBuzzTest {
 
-    @Nested
-    class theArgumentIsNotAMultipleOf3And5 {
-        FizzBuzz fizzBuzz = new FizzBuzz();
+    private FizzBuzz fizzBuzz = new FizzBuzz();
 
-        @Test
-        void postOneReturnOne() {
-            assertEquals("1", fizzBuzz.response(1));
-        }
+    @BeforeEach
+    void setup() {
+        fizzBuzz = new FizzBuzz();
     }
 
-    @Nested
-    class argumentMultipleOnlyThree {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        @Test
-        void postThreeReturnFizz() {
-            assertEquals("Fizz", fizzBuzz.response(3));
-        }
+    @Test
+    void postOneReturnOne() {
+        assertEquals("1", fizzBuzz.response(1));
     }
 
-    @Nested
-    class argumentMultipleOnlyFive {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        @Test
-        void postFiveReturnBuzz() {
-            assertEquals("Buzz", fizzBuzz.response(5));
-        }
+    @Test
+    void postThreeReturnFizz() {
+        assertEquals("Fizz", fizzBuzz.response(3));
     }
 
-    @Nested
-    class argumentMultipleThreeAndFive {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        @Test
-        void postFifteenReturnFizzBuzz() {
-            assertEquals("FizzBuzz", fizzBuzz.response(15));
-        }
+    @Test
+    void postFiveReturnBuzz() {
+        assertEquals("Buzz", fizzBuzz.response(5));
     }
 
-    @Nested
-    class argumentEffectiveBoundaryValue {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        @Test
-        void postOneReturnOne() {
-            assertEquals("1", fizzBuzz.response(1));
-        }
-
-        @Test
-        void postOneHundredReturnBuzz() {
-            assertEquals("Buzz", fizzBuzz.response(100));
-        }
+    @Test
+    void postFifteenReturnFizzBuzz() {
+        assertEquals("FizzBuzz", fizzBuzz.response(15));
     }
 
-    @Nested
-    class argumentInvalidBoundaryValue {
+    @Test
+    void postOneHundredReturnBuzz() {
+        assertEquals("Buzz", fizzBuzz.response(100));
+    }
+
+    @Test
+    void postZeroToError() {
+        assertThrows(IndexOutOfBoundsException.class, () -> fizzBuzz.response(0));
+    }
+
+    @Test
+    void postOneHundredOneError() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
-        @Test
-        void postZeroToError() {
-            assertThrows(IndexOutOfBoundsException.class, () -> {
-                fizzBuzz.response(0);
-            });
-        }
-
-        @Test
-        void postOneHundredOneError() {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-
-            assertThrows(IndexOutOfBoundsException.class, () -> {
-                fizzBuzz.response(101);
-            });
-        }
+        assertThrows(IndexOutOfBoundsException.class, () -> fizzBuzz.response(101));
     }
 }
